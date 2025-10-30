e0ef40108c4907210f57eb9ed4e9c24d6f030e5a
happyboy <superhappyboy1995@gmail.com>
2025-10-30 20:00:15 +0800

    update : Added global constants related to orca.

---
+++ src/global.rs
@@ -0,0 +1,46 @@
+// Orca Whirlpools (Concentrated Liquidity)
+pub const ORCA_WHIRLPOOLS_PROGRAM_ID: &str = "whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc";
+// Orca Stable Pools
+pub const ORCA_STABLE_SWAP_PROGRAM_ID: &str = "9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP";
+// Orca Standard Pools (Legacy)
+pub const ORCA_SWAP_PROGRAM_ID_V1: &str = "DjVE6JNiYqPL2QXyCUUh8rNjHrbz9hXHNYt99MQ59qw1";
+pub const ORCA_SWAP_PROGRAM_ID_V2: &str = "9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP";
+// Orca Aquafarms (Yield Farming)
+pub const ORCA_AQUAFARM_PROGRAM_ID: &str = "82yxjeMsvaURa4MbZZ7WZZHfobirZYkH1zF8fmeGtyaQ";
+// Orca Double Dip Staking
+pub const ORCA_DOUBLE_DIP_PROGRAM_ID: &str = "A7vUDErNPCTt9qrB6SSM4F6GkxzUe9d8P3cXSmRg4eY4";
+// Orca Governance
+pub const ORCA_GOVERNANCE_PROGRAM_ID: &str = "GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw";
+// Orca Vote Market
+pub const ORCA_VOTE_MARKET_PROGRAM_ID: &str = "votX1QehajS3TqV3yYe3QZJGMuQTjZ2d8nGtYX3dSq";
+// Orca Token Program IDs
+pub const ORCA_TOKEN_MINT: &str = "orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE";
+pub const ORCA_SOL_MINT: &str = "orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE";
+// Orca Associated Token Accounts
+pub const ORCA_ATA_PROGRAM_ID: &str = "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL";
+// Orca Fee Accounts
+pub const ORCA_FEE_ACCOUNT: &str = "CV4NfvT23j5xX73N3XqPxQ4Wi2J1GfzmHDvM2XWQZ4bU";
+pub const ORCA_COMMUNITY_FEE_ACCOUNT: &str = "CV4NfvT23j5xX73N3XqPxQ4Wi2J1GfzmHDvM2XWQZ4bU";
+// Orca Program Derived Addresses
+pub const ORCA_WHIRLPOOLS_CONFIG: &str = "2LecshUwdy9xi7meFgHtFJQNSKk4KdTrcpvaB56dP2NQ";
+pub const ORCA_FEE_AUTHORITY: &str = "3Jz4Q6ektbu5MkZq6yLmJcC8FcYmc7QyJt8ZvC7vj5rR";
+// Orca Pool Types
+pub const ORCA_POOL_TYPE_STABLE: u8 = 0;
+pub const ORCA_POOL_TYPE_VOLATILE: u8 = 1;
+pub const ORCA_POOL_TYPE_CONCENTRATED: u8 = 2;
+// Orca Instruction Discriminators
+pub const ORCA_INSTRUCTION_INITIALIZE: u8 = 0;
+pub const ORCA_INSTRUCTION_SWAP: u8 = 1;
+pub const ORCA_INSTRUCTION_DEPOSIT: u8 = 2;
+pub const ORCA_INSTRUCTION_WITHDRAW: u8 = 3;
+pub const ORCA_INSTRUCTION_ADD_LIQUIDITY: u8 = 4;
+pub const ORCA_INSTRUCTION_REMOVE_LIQUIDITY: u8 = 5;
+// SOL and USDC tokens mint
+pub const SOL_MINT: &str = "So11111111111111111111111111111111111111112";
+pub const USDC_MINT: &str = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
+pub const USDT_MINT: &str = "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB";
+pub const WSOL_MINT: &str = "So11111111111111111111111111111111111111112";
+// Token Program IDs
+pub const TOKEN_PROGRAM_ID: &str = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
+pub const ASSOCIATED_TOKEN_PROGRAM_ID: &str = "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL";
+pub const SYSTEM_PROGRAM_ID: &str = "11111111111111111111111111111111";
